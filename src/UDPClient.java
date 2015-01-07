9b6bf79ae88da0931e3e7e5a133de5c74cd76327
Leonard Hecker <leonard@hecker.io>
2015-01-07 14:27:58 +0100

    Initial commit.

---
+++ src/UDPClient.java
@@ -0,0 +1,258 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.SocketTimeoutException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.zip.CRC32;
+
+class UDPClient {
+	private static final byte PACKET_ID_MAX = 1;
+	private static final byte PACKET_RESEND_MAX = 10;
+
+	private static ByteBuffer _rxd = ByteBuffer.allocate(64 * 1024);
+	private static DatagramPacket _rxp = new DatagramPacket(_rxd.array(), _rxd.capacity());
+	private static DatagramSocket _socket;
+	private static InetSocketAddress _targetAddress;
+
+	private static short _sessionId = 0;
+	private static byte _packetId = PACKET_ID_MAX;
+
+	private static long _totalBytes;
+	private static AtomicLong _finishedBytes = new AtomicLong();
+	private static long _previousBytes;
+	private static long _previousTime;
+
+	static {
+		Random rand = new Random();
+		_sessionId = (short) rand.nextInt(0x10000);
+	}
+
+	private static byte getNextPacketId() {
+		_packetId = (byte) ((_packetId + 1) % (PACKET_ID_MAX + 1));
+		return _packetId;
+	}
+
+	private static String formatSize(double size) {
+		final String prefixes = "kMGTPE";
+		double sizeExp = Math.floor(Math.log(size) / Math.log(1000));
+		String sizePrefix;
+
+		try {
+			int idx = (int) sizeExp;
+			sizePrefix = prefixes.substring(idx - 1, idx);
+			return String.format("%.2f%sB", size / Math.pow(1000, sizeExp), sizePrefix);
+		} catch (StringIndexOutOfBoundsException e) {
+			return String.format("%.0fB", size);
+		}
+	}
+
+	private static void showStats() {
+		final long time = System.nanoTime();
+		final long finishedBytes = _finishedBytes.get();
+		final double finishedDelta = finishedBytes - _previousBytes;
+		final double timeDelta = time - _previousTime;
+		final double speed = 1e9 * finishedDelta / timeDelta;
+		final double percent = (double) finishedBytes / _totalBytes;
+
+		final int barWidth = (int) Math.round(percent * 50);
+		final char[] barData = new char[50];
+		Arrays.fill(barData, 0, barWidth, '=');
+
+		if (barWidth > 0 && barWidth < 50) {
+			barData[barWidth] = '>';
+		}
+
+		final String bar = new String(barData);
+		int etaSec = (int) ((_totalBytes - finishedBytes) / speed);
+		int etaMin;
+
+		if (etaSec < 0 || etaSec == Integer.MAX_VALUE) {
+			etaMin = 0;
+			etaSec = 0;
+		} else {
+			etaMin = etaSec / 60;
+			etaSec %= 60;
+		}
+
+		System.out.printf("\r%3.0f%% [%s] %,d  %s/s  eta %dm %ds", percent * 100, bar, finishedBytes, UDPClient.formatSize(speed), etaMin, etaSec);
+
+		_previousTime = time;
+		_previousBytes = finishedBytes;
+	}
+
+	private static void send(ByteBuffer txd) throws IOException {
+		DatagramPacket txp = new DatagramPacket(txd.array(), txd.limit(), _targetAddress);
+		int i = 0;
+
+		while (true) {
+			try {
+				_socket.send(txp);
+
+				_rxd.clear();
+				_socket.receive(_rxp);
+				_rxd.limit(_rxp.getLength());
+
+				if (_rxd.limit() != 3) {
+					throw new IOException("ACK: invalid size");
+				}
+
+				short sessionId = _rxd.getShort();
+				byte packetId = _rxd.get();
+
+				if (sessionId != _sessionId) {
+					throw new IOException("ACK: invalid session id");
+				}
+
+				if (packetId != _packetId) {
+					throw new IOException("ACK: invalid packet id");
+				}
+
+				break;
+			} catch (SocketTimeoutException e) {
+				if (++i == PACKET_RESEND_MAX) {
+					throw e;
+				}
+			}
+		}
+	}
+
+	public static void main(String args[]) throws Exception {
+		if (args.length != 3) {
+			System.out.println("Usage: client-udp [host] [port] [filepath]");
+			return;
+		}
+
+		_socket = new DatagramSocket();
+		_socket.setSoTimeout(1000);
+		_targetAddress = new InetSocketAddress(args[0], Short.parseShort(args[1]));
+
+		if (_targetAddress.isUnresolved()) {
+			System.err.println("[error] Cannot resolve: " + args[0] + ':' + args[1]);
+			return;
+		}
+
+		final Enumeration<NetworkInterface> inets = NetworkInterface.getNetworkInterfaces();
+		int mtu = Integer.MAX_VALUE;
+
+		/*
+		 *  TODO: Improve the precision of mtu by getting the MTU 
+		 *  of the NIC the packets are going to be sent on.
+		 */
+		while (inets.hasMoreElements()) {
+			int newMtu = inets.nextElement().getMTU();
+
+			if (newMtu != -1 && newMtu < mtu) {
+				mtu = newMtu;
+			}
+		}
+
+		if (mtu == Integer.MAX_VALUE) {
+			mtu = 1500;
+		}
+
+		// As per RFC 791:
+		// "Every internet module must be able to forward a datagram of 68 octets without further fragmentation."
+		if (mtu < 68) {
+			System.err.println("[error] MTU smaller than required IPv4 minimum size.");
+			return;
+		}
+
+		final File file = new File(args[2]);
+
+		if (!file.isFile()) {
+			System.err.println("[error] File not found or not readable: " + args[2]);
+			return;
+		}
+
+		final String filename = file.getName();
+		final byte[] filenameData = filename.getBytes("UTF-8");
+
+		if (filenameData.length > 255) {
+			System.err.println("[error] Filename too long: '" + filename + "'");
+			return;
+		}
+		
+		_totalBytes = file.length();
+
+		// 40 Byte IPv6 Header size + 8 Byte UDP Header size
+		mtu -= 48;
+
+		// Java's CRC32 uses the IEEE 0x04C11DB7 polynomial
+		final CRC32 cc = new CRC32();
+		final Timer timer = new Timer();
+		ByteBuffer txd;
+
+		try (final FileInputStream fin = new FileInputStream(file)) {
+			System.out.print("Connecting to " + _targetAddress.getAddress().getHostAddress() + ":" + _targetAddress.getPort() + "... ");
+
+			// the handshake header fields
+			txd = ByteBuffer.allocate(2 + 1 + 5 + 8 + 2 + filenameData.length + 4);
+			txd.putShort(_sessionId);
+			txd.put(UDPClient.getNextPacketId());
+			txd.put(new byte[] { 'S', 't', 'a', 'r', 't' });
+			txd.putLong(_totalBytes);
+			txd.putShort((short) filenameData.length);
+			txd.put(filenameData);
+
+			cc.update(txd.array(), 0, txd.position());
+
+			txd.putInt((int) cc.getValue());
+
+			UDPClient.send(txd);
+
+			System.out.printf("connected.%nSending: '%s'%nLength: %,d (%s)%n%n", filename, _totalBytes, UDPClient.formatSize(_totalBytes));
+
+			cc.reset();
+			txd = ByteBuffer.allocate(mtu);
+
+			_previousTime = System.nanoTime();
+
+			timer.schedule(new TimerTask() {
+				public void run() {
+					UDPClient.showStats();
+				}
+			}, 1000, 1000);
+
+			while (true) {
+				txd.clear();
+				txd.putShort(_sessionId);
+				txd.put(UDPClient.getNextPacketId());
+
+				int n = fin.read(txd.array(), txd.position(), txd.remaining());
+
+				if (n == -1) {
+					break;
+				}
+
+				cc.update(txd.array(), txd.position(), n);
+				txd.limit(txd.position() + n);
+
+				_finishedBytes.addAndGet(n);
+
+				UDPClient.send(txd);
+			}
+
+			fin.close();
+
+			txd.putInt((int) cc.getValue());
+			txd.flip();
+			send(txd);
+
+			UDPClient.showStats();
+		} catch (Exception e) {
+			System.err.println("[error] " + e.getMessage());
+		} finally {
+			timer.cancel();
+		}
+	}
+}
---
+++ src/UDPServer.java
@@ -0,0 +1,269 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketAddress;
+import java.net.SocketTimeoutException;
+import java.nio.ByteBuffer;
+import java.util.zip.CRC32;
+
+class UDPServer {
+	private static final byte PACKET_ID_MAX = 1;
+	private static final byte PACKET_RESEND_MAX = 10;
+
+	private static ByteBuffer _rxd = ByteBuffer.allocate(64 * 1024);
+	private static DatagramPacket _rxp = new DatagramPacket(_rxd.array(), _rxd.capacity());
+	private static DatagramSocket _socket;
+	private static byte _packetId = PACKET_ID_MAX;
+
+	private static byte getNextPacketId() {
+		_packetId = (byte) ((_packetId + 1) % (PACKET_ID_MAX + 1));
+		return _packetId;
+	}
+
+	private static void receive() throws IOException {
+		int i = 0;
+
+		while (true) {
+			try {
+				_rxd.clear();
+				_socket.receive(_rxp);
+				_rxd.limit(_rxp.getLength());
+				return;
+			} catch (SocketTimeoutException e) {
+				if (++i == PACKET_RESEND_MAX) {
+					throw e;
+				}
+			}
+		}
+	}
+
+	private static void sendACK(SocketAddress address, short sessionId, byte packetId) throws IOException {
+		ByteBuffer txd = ByteBuffer.allocate(3);
+		txd.putShort(sessionId);
+		txd.put(packetId);
+
+		DatagramPacket packet = new DatagramPacket(txd.array(), txd.capacity(), address);
+		_socket.send(packet);
+	}
+
+	private static File discoverFile(byte[] f) throws Exception {
+		String filename = new String(f, "UTF-8");
+		File file = new File(filename);
+
+		if (file.exists()) {
+			int idx = filename.lastIndexOf('.');
+			String name = filename.substring(0, idx);
+			String ext = filename.substring(idx);
+
+			for (int i = 1; i < Integer.MAX_VALUE; i++) {
+				file = new File(name + String.valueOf(i) + ext);
+
+				if (!file.exists()) {
+					break;
+				}
+			}
+
+			if (file.exists()) {
+				throw new Exception("could not find non existing filename");
+			}
+		}
+
+		return file;
+	}
+
+	public static void main(String args[]) throws Exception {
+		if (args.length != 1) {
+			System.out.println("Usage: server-udp [port]");
+			return;
+		}
+
+		_socket = new DatagramSocket(Integer.parseInt(args[0]));
+
+		// Java's CRC32 uses the IEEE 0x04C11DB7 polynomial
+		final CRC32 cc = new CRC32();
+		final byte[] start = { 'S', 't', 'a', 'r', 't' };
+
+		mainloop:
+		while (true) {
+			_socket.setSoTimeout(0);
+			UDPServer.receive();
+
+			final SocketAddress h_address = _rxp.getSocketAddress();
+
+			// TODO: implement dynamic timeouts
+			_socket.setSoTimeout(1000);
+			
+			// reset ---> the next call to getNextPacketId() will return 0
+			_packetId = PACKET_ID_MAX;
+
+			/*
+			 * The handshake header fields:
+			 *   short h_sessionId;
+			 *   byte h_packetId;
+			 *   long h_length;
+			 *   short h_filenameLength;
+			 *   byte[] h_filename;
+			 *   int h_crc32;
+			 */
+
+			/*
+			 * The handshake packet is in every possible case at least 23 Bytes
+			 * large. (This includes +1 for the assertion, that the filename is
+			 * at least 1 Byte long.)
+			 */
+			if (_rxd.limit() < 23) {
+				System.err.println("[error] handshake: too small");
+				continue mainloop;
+			}
+
+			final short h_sessionId = _rxd.getShort();
+			final byte h_packetId = _rxd.get();
+
+			// as per specification the handshake must have a packet ID of 0
+			if (h_packetId != UDPServer.getNextPacketId()) {
+				System.err.println("[error] handshake: invalid packet id");
+				continue mainloop;
+			}
+
+			for (int i = 0; i < start.length; i++) {
+				if (_rxd.get() != start[i]) {
+					System.err.println("[error] handshake: invalid \"Start\" signature");
+					continue mainloop;
+				}
+			}
+
+			final long h_length = _rxd.getLong();
+
+			if (h_length <= 0) {
+				System.err.println("[error] handshake: invalid (zero) or too large (greater than Long.MAX_VALUE) length field");
+				continue mainloop;
+			}
+
+			final short h_filenameLength = _rxd.getShort();
+
+			/*
+			 * Check remaining() if the filename is actually fully present,
+			 * including 4 additional Bytes for the CRC32.
+			 */
+			if (h_filenameLength <= 0 || _rxd.remaining() - 4 < h_filenameLength) {
+				System.err.println("[error] handshake: invalid filename field");
+				continue mainloop;
+			}
+
+			final byte[] h_filename = new byte[h_filenameLength];
+			_rxd.get(h_filename);
+
+			cc.reset();
+			cc.update(_rxd.array(), 0, _rxd.position());
+
+			final int h_crc32 = _rxd.getInt();
+
+			/*
+			 * Casting cc.getValue() down to int is very important.
+			 * cc.getValue() will return some positive value [0, 2^32).
+			 * _rxd.getInt() will return the same value (bitwise), but in a signed representation.
+			 * Thus we can just cast the first one down to an int, to get a correct comparison.
+			 * If we don't, the compiler would promote the (int) h_crc32 to an (long),
+			 * which turns (int)-1 to (long)-1, instead of an positive value [0, 2^32). 
+			 */
+			if (h_crc32 != (int) cc.getValue()) {
+				System.err.println("[error] handshake: invalid checksum");
+				continue mainloop;
+			}
+
+			UDPServer.sendACK(h_address, h_sessionId, h_packetId);
+
+			final File file = UDPServer.discoverFile(h_filename);
+
+			try (final FileOutputStream fout = new FileOutputStream(file)) {
+				long remaining = h_length;
+
+				cc.reset();
+
+				while (remaining >= 0) {
+					short d_sessionId;
+					byte d_packetId;
+					int d_crc32;
+
+					try {
+						UDPServer.receive();
+					} catch (SocketTimeoutException e) {
+						throw new Exception("timeout");
+					}
+
+					SocketAddress d_address = _rxp.getSocketAddress();
+
+					if (!d_address.equals(h_address)) {
+						System.err.println("[warning] data: interference from another client " + d_address.toString());
+						continue;
+					}
+
+					/*
+					 * +1 byte as an assertion that this packet
+					 * contains at least a single byte of data.
+					 */
+					if (_rxd.limit() < 4) {
+						throw new Exception("too small");
+					}
+
+					d_sessionId = _rxd.getShort();
+					d_packetId = _rxd.get();
+
+					if (d_sessionId != h_sessionId) {
+						throw new Exception("invalid session id");
+					}
+
+					if (d_packetId != UDPServer.getNextPacketId()) {
+						throw new Exception("invalid packet id");
+					}
+
+					int dataLength = _rxd.remaining();
+
+					/*
+					 * If remaining is -4 this packet must contain additional 4
+					 * bytes at the end, which contains the CRC32 sum over all data.
+					 * If remaining is 0 the next packet must only contain 4 bytes
+					 * at the end, which contains the CRC32 sum over all data.
+					 */
+					remaining -= dataLength;
+
+					if (remaining <= 0) {
+						if (remaining == -4) {
+							dataLength -= 4;
+						} else if (remaining != 0) {
+							throw new Exception("missing final CRC32");
+						}
+					}
+
+					if (dataLength > 0) {
+						cc.update(_rxd.array(), _rxd.position(), dataLength);
+						fout.write(_rxd.array(), _rxd.position(), dataLength);
+					}
+
+					if (remaining == -4) {
+						d_crc32 = _rxd.getInt(_rxd.position() + dataLength);
+
+						/*
+						 * Casting cc.getValue() down to int is very important.
+						 * cc.getValue() will return some positive value [0, 2^32).
+						 * _rxd.getInt() will return the same value (bitwise), but in a signed representation.
+						 * Thus we can just cast the first one down to an int, to get a correct comparison.
+						 * If we don't, the compiler would promote the (int) h_crc32 to an (long),
+						 * which turns (int)-1 to (long)-1, instead of an positive value [0, 2^32). 
+						 */
+						if (d_crc32 != (int) cc.getValue()) {
+							throw new Exception("checksum not equal");
+						}
+					}
+
+					UDPServer.sendACK(h_address, d_sessionId, d_packetId);
+				}
+			} catch (Exception e) {
+				System.err.println("[error] data: " + e.getMessage());
+				file.delete();
+			}
+		}
+	}
+}
